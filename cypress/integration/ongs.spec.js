c90166075d13b343bc6caea0fe2e443e8b0dc9c0
Jonathan Alves <jonathansilvaalves16@gmail.com>
2020-10-07 00:55:04 -0300

    Add news tests in layout

--- cypress/integration/ongs.spec.js
+++ cypress/integration/ongs.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Ongs', () => {
-    it('devem poder ', () => {
+    it('devem poder realizar um cadastro ', () => {
         cy.visit('http://localhost:3000/register')
         cy.get("[placeholder='Nome da ONG']").type('Dogs queridos');
         cy.get("[type='email']").type('dogs@mail.com');
@@ -23,8 +23,44 @@ describe('Ongs', () => {
     it('deve poder realizar um login no sistema', () => {
 
         cy.visit('http://localhost:3000/');
-        cy.get('input').type(Cypress.env('createdOngId'));
+        cy.get('[data-cy=id]').type(Cypress.env('createdOngId'));
+        cy.get('[data-cy=button-login]').click();
+
+    });
+
+    it.skip('devem poder fazer logout', () => {
+        cy.login();
+        cy.get('button').click();
+    });
+
+    it('devem poder cadastrar novos casos', () => {
+        cy.login();
         cy.get('.button').click();
+        cy.get('[placeholder="Título do caso"]').type('Mary Cat 2');
+        cy.get('textarea').type('Apoio aos animais');
+        cy.get('[placeholder="Valor em reais"]').type('400');
+
+        cy.route('POST', '**/incidents').as('newIncident');
+        cy.get('.button').click();
+
+        cy.wait('@newIncident').then((xhr) => {
+            expect(xhr.status).to.eq(200);
+            expect(xhr.response.body).has.property('id');
+            expect(xhr.response.body.id).is.not.null;
+        })
+    });
+
+    it('devem poder excluir um caso', () => {
+        cy.createNewIncident();
+        cy.login();
+
+        cy.route('DELETE', '**/incidents/*').as('deleteIncident');
+        cy.get('li > button > svg').click();
+
+        cy.wait('@deleteIncident').then((xhr) => {
+            expect(xhr.status).to.eq(204);
+            expect(xhr.response.body).to.be.empty;
+        });
 
     });
 })
